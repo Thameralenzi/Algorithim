1769b6c242d151f8c9677d5a74a81e5f66004ed8
Thamer A Sh K Hasoon <thameradel96@gmail.com>
2025-01-26 20:57:06 +0000

    adding extension method check prime number
    The extension adds a new method called check_prime to check if two numbers are coprime. Two numbers are coprime if their GCD is 1, meaning they don’t share any divisors except 1

--- EuclideanAlgorithm.py
+++ EuclideanAlgorithm.py
@@ -9,7 +9,9 @@ class GCDCalculator:
             y = x % y  # calculate the remainder
             x = temp  # update 'x' to the previous value of 'y'
         return x
-    
+    def check_prime(x, y):
+        """this method checks if two numbers have a gcd of 1"""
+        return GCDCalculator.calculate_gcd(x, y) == 1
 # # let us try our own selves
 # try first input 100 and the second input 80 for example
 def get_input():
@@ -29,3 +31,9 @@ input_data = get_input()
 if input_data != 0:
     gcd_result = GCDCalculator.calculate_gcd(input_data[0], input_data[1])
     print(f"the gcd of {input_data[0]} and {input_data[1]} is: {gcd_result}")
+    
+    # check if the numbers have a gcd of 1
+    if GCDCalculator.check_prime(input_data[0], input_data[1]):
+        print(f"{input_data[0]} and {input_data[1]} have a gcd of 1.")
+    else:
+        print(f"{input_data[0]} and {input_data[1]} do not have a gcd of 1.")
